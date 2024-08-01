39d03883eab810a62577cfa57277bb6ddda31702
at-robins <30956028+at-robins@users.noreply.github.com>
2024-08-01 10:13:41 +0200

    Added dummy voronoi module
    A WIP voronoi module has been added.

--- src/input.rs
+++ src/input.rs
@@ -1,15 +1,177 @@
 //! This module handles parsing of input data.
 
-use getset::Getters;
+use core::f64;
+use std::{borrow::Borrow, collections::HashSet};
+
+use geo::{BoundingRect, LineString, Polygon};
+use getset::{CopyGetters, Getters};
 use serde::{Deserialize, Serialize};
 
 /// A set of 2d points bound by a specified polygon.
 #[derive(Getters, Clone, Debug, Deserialize, Serialize)]
 pub struct BoundedPointSet {
     /// The set of 2d points.
-    #[getset(get = "pub")]
     point_set: Vec<[f64; 2]>,
     /// The bounding polygon.
     #[getset(get = "pub")]
     bound: Vec<[f64; 2]>,
+}
+
+impl BoundedPointSet {
+    /// Returns the bounding polygon or an error if less than 3 points have been specified.
+    pub fn bounding_polygon(&self) -> Result<Polygon, &'static str> {
+        let bound_points: Vec<(f64, f64)> = self
+            .bound()
+            .into_iter()
+            .map(|point| (point[0], point[1]))
+            .collect();
+        if bound_points.len() < 3 {
+            Err("At least 3 points are needed to specify a bounding polygon.")
+        } else {
+            Ok(Polygon::new(LineString::from(bound_points), Vec::new()))
+        }
+    }
+
+    /// The set of unique, filtered 2d points.
+    pub fn point_set(&self) -> HashSet<Point2D> {
+        (&self.point_set)
+            .into_iter()
+            .filter_map(|point| Point2D::new(point[0], point[1]))
+            .collect()
+    }
+
+    /// Returns the point set as unique set of [`points`](voronoice::Point).
+    pub fn voronoi_point_set(&self) -> Vec<voronoice::Point> {
+        self.point_set()
+            .into_iter()
+            .map(|point| point.into())
+            .collect()
+    }
+}
+
+#[derive(Clone, Copy, CopyGetters, Debug, PartialEq, PartialOrd)]
+pub struct Point2D {
+    #[getset(get_copy = "pub")]
+    x: f64,
+    #[getset(get_copy = "pub")]
+    y: f64,
+}
+
+impl Point2D {
+    /// Tries to create a new 2 dimensional point from
+    /// the specified coordinates.
+    /// Fails if one of the specified coordinates is not normal.
+    ///
+    /// # Parameters
+    ///
+    /// * `x` - the x coordinate
+    /// * `y` - the y coordinate
+    pub fn new(x: f64, y: f64) -> Option<Self> {
+        if x.is_normal() || y.is_normal() {
+            None
+        } else {
+            Some(Point2D { x, y })
+        }
+    }
+}
+
+// Eq and Ord can be implemented as all non-normal
+// coordinates have been filtered out during creation.
+impl Eq for Point2D {}
+
+impl Ord for Point2D {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+impl std::hash::Hash for Point2D {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.x.to_bits().hash(state);
+        self.y.to_bits().hash(state);
+    }
+}
+
+impl From<Point2D> for voronoice::Point {
+    fn from(value: Point2D) -> Self {
+        voronoice::Point {
+            x: value.x(),
+            y: value.y(),
+        }
+    }
+}
+
+#[derive(Clone, Copy, CopyGetters, Debug, PartialEq, PartialOrd)]
+pub struct Bounds {
+    #[getset(get_copy = "pub")]
+    min_x: f64,
+    #[getset(get_copy = "pub")]
+    max_x: f64,
+    #[getset(get_copy = "pub")]
+    min_y: f64,
+    #[getset(get_copy = "pub")]
+    max_y: f64,
+}
+
+impl Bounds {
+    pub fn from_polygon<T: Borrow<Polygon>>(polygon: T) -> Option<Self> {
+        polygon.borrow().bounding_rect().map(|bound| {
+            let (min_x, min_y) = bound.min().x_y();
+            let (max_x, max_y) = bound.max().x_y();
+            Self {
+                min_x,
+                max_x,
+                min_y,
+                max_y,
+            }
+        })
+    }
+
+    pub fn from_point_set<T: Borrow<HashSet<Point2D>>>(point_set: T) -> Option<Self> {
+        let point_set: &HashSet<Point2D> = point_set.borrow();
+        if point_set.is_empty() {
+            None
+        } else {
+            let mut min_x = f64::MAX;
+            let mut max_x = f64::MIN;
+            let mut min_y = f64::MAX;
+            let mut max_y = f64::MIN;
+
+            for point in point_set {
+                if min_x > point.x() {
+                    min_x = point.x();
+                }
+                if max_x < point.x() {
+                    max_x = point.x();
+                }
+                if min_y > point.y() {
+                    min_y = point.y();
+                }
+                if max_y < point.y() {
+                    max_y = point.y();
+                }
+            }
+            Some(Self {
+                min_x,
+                max_x,
+                min_y,
+                max_y,
+            })
+        }
+    }
+
+    pub fn diff_x(&self) -> f64 {
+        self.max_x - self.min_x
+    }
+
+    pub fn diff_y(&self) -> f64 {
+        self.max_y - self.min_y
+    }
+
+    pub fn centre_x(&self) -> f64 {
+        self.min_x() + self.diff_x() / 2.0
+    }
+    pub fn centre_y(&self) -> f64 {
+        self.min_y() + self.diff_y() / 2.0
+    }
 }
---
+++ src/voronoi.rs
@@ -0,0 +1,75 @@
+//! This module computes the bounded voronoi diagramm.
+
+use std::borrow::Borrow;
+
+use geo::{BoundingRect, LineString, Polygon};
+use voronoice::{BoundingBox, VoronoiBuilder};
+
+use crate::input::{BoundedPointSet, Bounds};
+
+pub fn compute_voronoi<T: Borrow<BoundedPointSet>>(
+    bounded_point_set: T,
+) -> Result<Vec<BoundedVoronoiCell>, &'static str> {
+    let bounded_point_set: &BoundedPointSet = bounded_point_set.borrow();
+
+    let bound = bounded_point_set.bounding_polygon()?;
+    let bound_bounds = Bounds::from_polygon(&bound).ok_or("The bounding polygon is invalid.")?;
+    let bound_point_set = Bounds::from_point_set(&bounded_point_set.point_set())
+        .ok_or("The point set does not contain enough valid points.")?;
+
+    let sites = bounded_point_set.voronoi_point_set();
+    let voronoi_digramm = VoronoiBuilder::default()
+        .set_sites(sites)
+        .set_bounding_box(BoundingBox::new(
+            voronoice::Point {
+                x: bound_point_set.centre_x(),
+                y: bound_point_set.centre_y(),
+            },
+            bound_point_set.diff_x() + bound_bounds.diff_x(),
+            bound_point_set.diff_y() + bound_bounds.diff_y(),
+        ))
+        .set_lloyd_relaxation_iterations(5)
+        .build()
+        .ok_or("No Voronoi diagramm could be built for the specified point set.")?;
+
+    let cells = voronoi_digramm
+        .iter_cells()
+        .map(|cell| BoundedVoronoiCell {
+            site: voronoi_point_to_array(cell.site_position()),
+            cell: cell.iter_vertices().map(voronoi_point_to_array).collect(),
+        })
+        .collect();
+    Ok(cells)
+}
+
+/// Centers the polygon around the specified coordinates.
+///
+/// # Parameters
+///
+/// * `polygon` - the input polygon to center
+/// * `x` - the x-coordinate of the new center
+/// * `y` - the y-coordinate of the new center
+fn center_polygon<T: Borrow<Polygon>>(polygon: T, x: f64, y: f64) -> Result<Polygon, &'static str> {
+    let polygon: &Polygon = polygon.borrow();
+    let centre = polygon
+        .bounding_rect()
+        .ok_or("Invalid polygon. Cannot calculate bounding rectangle.")?
+        .center();
+    let dif_x = centre.x - x;
+    let dif_y = centre.y - y;
+    let points: Vec<(f64, f64)> = polygon
+        .exterior()
+        .points()
+        .map(|point| (point.x() - dif_x, point.y() - dif_y))
+        .collect();
+    Ok(Polygon::new(LineString::from(points), Vec::new()))
+}
+
+pub struct BoundedVoronoiCell {
+    site: [f64; 2],
+    cell: Vec<[f64; 2]>,
+}
+
+fn voronoi_point_to_array(point: &voronoice::Point) -> [f64; 2] {
+    [point.x, point.y]
+}
